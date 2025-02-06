a6c91a040401dd84c5603c94115b11c65bc02d53
Dasuri330 <151821656+Dasuri330@users.noreply.github.com>
2025-02-06 22:30:18 +0800

    Add files via upload

---
+++ PM&JI Reservify System (Website)/customer_feedback.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Feedbacks</title>
+    <link rel="stylesheet" href="customer_feedback.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+</head>
+
+<body>
+
+<button class="back-button" onclick="window.history.back()">Back</button>
+    <!-- Feedback Section -->
+    <section id="testimonials">
+
+        <!-- Heading -->
+        <div class="testimonial-heading">
+            <span>Comments</span>
+            <h1>Customer says</h1>
+        </div>
+
+        <!-- Testimonial Box Container -->
+        <div class="testimonial-box-container">
+
+            <!-- Testimonial Box -->
+            <div class="testimonial-box">
+                <div class="box-top">
+                    <div class="profile">
+                        <!-- Profile Image -->
+                        <div class="profile-img">
+                            <img src="images/cat tuxedo.jpg" alt="Profile Image" />
+                        </div>
+                        <!-- Name and Username -->
+                        <div class="name-user">
+                            <strong>Elon Musk</strong>
+                            <span>@elonmusk@gmail.com</span>
+                        </div>
+                    </div>
+                    <!-- Reviews (Stars) -->
+                    <div class="reviews">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="far fa-star"></i> <!-- Empty star -->
+                    </div>
+                </div>
+                <!-- Comments -->
+                <div class="client-comment">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla velit earum repellendus quo voluptatum aliquid rem, praesentium quam sed recusandae quos cumque eaque. Dolore quasi quas, adipisci delectus iure facere?</p>
+                </div>
+            </div>
+
+            <!-- Testimonial Box (Duplicated) -->
+            <div class="testimonial-box">
+                <div class="box-top">
+                    <div class="profile">
+                        <!-- Profile Image -->
+                        <div class="profile-img">
+                            <img src="images/cat tuxedo.jpg" alt="Profile Image" />
+                        </div>
+                        <!-- Name and Username -->
+                        <div class="name-user">
+                            <strong>Elon Musk</strong>
+                            <span>@elonmusk@gmail.com</span>
+                        </div>
+                    </div>
+                    <!-- Reviews (Stars) -->
+                    <div class="reviews">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="far fa-star"></i> <!-- Empty star -->
+                        <i class="far fa-star"></i>
+                        <i class="far fa-star"></i> 
+                    </div>
+                </div>
+                <!-- Comments -->
+                <div class="client-comment">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla velit earum repellendus quo voluptatum aliquid rem, praesentium quam sed recusandae quos cumque eaque. Dolore quasi quas, adipisci delectus iure facere?</p>
+                </div>
+            </div>
+
+
+               <!-- Testimonial Box (Duplicated) -->
+               <div class="testimonial-box">
+                <div class="box-top">
+                    <div class="profile">
+                        <!-- Profile Image -->
+                        <div class="profile-img">
+                            <img src="images/cat tuxedo.jpg" alt="Profile Image" />
+                        </div>
+                        <!-- Name and Username -->
+                        <div class="name-user">
+                            <strong>Elon Musk</strong>
+                            <span>@elonmusk@gmail.com</span>
+                        </div>
+                    </div>
+                    <!-- Reviews (Stars) -->
+                    <div class="reviews">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="far fa-star"></i> <!-- Empty star -->
+                        <i class="far fa-star"></i> <!-- Empty star -->
+                    </div>
+                </div>
+                <!-- Comments -->
+                <div class="client-comment">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla velit earum repellendus quo voluptatum aliquid rem, praesentium quam sed recusandae quos cumque eaque. Dolore quasi quas, adipisci delectus iure facere?</p>
+                </div>
+            </div>
+
+               <!-- Testimonial Box (Duplicated) -->
+               <div class="testimonial-box">
+                <div class="box-top">
+                    <div class="profile">
+                        <!-- Profile Image -->
+                        <div class="profile-img">
+                            <img src="images/cat tuxedo.jpg" alt="Profile Image" />
+                        </div>
+                        <!-- Name and Username -->
+                        <div class="name-user">
+                            <strong>Elon Musk</strong>
+                            <span>@elonmusk@gmail.com</span>
+                        </div>
+                    </div>
+                    <!-- Reviews (Stars) -->
+                    <div class="reviews">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i> 
+                    </div>
+                </div>
+                <!-- Comments -->
+                <div class="client-comment">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla velit earum repellendus quo voluptatum aliquid rem, praesentium quam sed recusandae quos cumque eaque. Dolore quasi quas, adipisci delectus iure facere?</p>
+                </div>
+            </div>
+    </section>
+    <div class="centered-link">
+    <a href="add_reviews1.php">Add feedback</a>
+</div>
+</body>
+
+</html>
